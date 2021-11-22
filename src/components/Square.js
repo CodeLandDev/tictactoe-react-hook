ea8efafe35f935e6fe873a0321c93bc8b5ad8615
Aran <fatemerahmanyfr@gmail.com>
2021-11-22 23:54:24 +0330

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Square = (props) => {
+  const style = props.value ? `squares ${props.value}` : `squares`;
+
+  return (
+    <button className={style} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
+
+export default Square;
+
